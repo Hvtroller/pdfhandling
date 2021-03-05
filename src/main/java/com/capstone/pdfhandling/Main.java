cddf2fe9cee5a934067c2b8c9200317a55c6fb6c
Hvtroller <34528358+Hvtroller@users.noreply.github.com>
2021-03-05 17:49:15 +0700

    disabled openaction in pdf
    changed the code, using new library to disable open action in pdf files. The JS code not remove.

--- src/main/java/com/capstone/pdfhandling/Main.java
+++ src/main/java/com/capstone/pdfhandling/Main.java
@@ -17,6 +17,6 @@ public class Main {
 
     public static void main(String[] args) {
         PdfHandling handler = new PdfHandling();
-        handler.printText("js.pdf");
+        handler.sanitize("input/bigjs.pdf");
     }
 }
